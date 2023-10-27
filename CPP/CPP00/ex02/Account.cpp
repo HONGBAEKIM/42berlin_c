5f8e99f245f7d75dddce63374f2a560257adfa81
HONGBAEKIM <hongpei17@gmail.com>
2023-10-27 10:43:17 +0200

    C00_ex02_just print all the functions from Account.hpp

--- CPP/CPP00/ex02/Account.cpp
+++ CPP/CPP00/ex02/Account.cpp
@@ -10,5 +10,73 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+# include <string>
+
+//std::cout, std::endl
+# include <iostream>
+
+//std::setw
+# include <iomanip>
+
+//isalpha
+# include <cctype>
+
+//for std::exit
+#include <cstdlib>
+
 //Output matches perfectly whit log file (except for the timestamps)
+
+void    Account::_displayTimestamp()
+{
+
+}
+
+
+void    Account::displayAccountsInfos()
+{
+
+}
+
+
+Account::Account( int initial_deposit )
+{
+    //std::cout << "Account" << std::endl;
+    std::cout << initial_deposit << std::endl;
+}
+
+
+
+void    Account::makeDeposit( int deposit )
+{
+    std::cout << deposit << std::endl;
+
+}
+
+bool    Account::makeWithdrawal( int withdrawal )
+{
+    // if (getNbWithdrawals <= getTotalAmount)
+    // {
+    //     getTotalAmount -= getNbWithdrawals;
+    //     return (withdrawal = true);
+    // }
+    // else
+    //     return (withdrawal = false);
+    return (std::cout << "withdrawal" << withdrawal << std::endl);
+
+}
+
+// int    Account::checkAmount( void ) const {}
+
+void    Account::displayStatus( void ) const
+{
+
+}
+
+Account::~Account()
+{
+
+}
+
 
