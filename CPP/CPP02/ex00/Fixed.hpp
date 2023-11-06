209de1be3cbf9ebb7a2af323cbeec1dd76e6d844
Hongbae Kim <hongbaki@c4c6c3.42berlin.de>
2023-11-06 12:03:18 +0100

    implement CPP/CPP02/ex00

--- CPP/CPP02/ex00/Fixed.hpp
+++ CPP/CPP02/ex00/Fixed.hpp
@@ -6,7 +6,37 @@
 /*   By: hongbaki <hongbaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 19:25:25 by hongbaki          #+#    #+#             */
-/*   Updated: 2023/10/12 19:25:26 by hongbaki         ###   ########.fr       */
+/*   Updated: 2023/11/06 10:27:59 by hongbaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_H
+# define FIXED_H
+
+#include <iostream>
+#include <string>
+
+class Fixed
+{
+    private:
+        int value;
+        static const int bits;
+        
+    public:
+        //Default constructor
+        Fixed();
+        
+        //Copy constructor
+        Fixed(const Fixed &_fixed);
+        
+        //Copy assignment operator
+        Fixed &operator=(const Fixed &_fixed);
+        
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        
+        //Destructor
+        ~Fixed();
+};
+
+#endif
--- CPP/CPP02/ex00/main.cpp
+++ CPP/CPP02/ex00/main.cpp
@@ -6,7 +6,23 @@
 /*   By: hongbaki <hongbaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 19:25:51 by hongbaki          #+#    #+#             */
-/*   Updated: 2023/10/12 19:25:59 by hongbaki         ###   ########.fr       */
+/*   Updated: 2023/11/03 09:33:05 by hongbaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main( void ) 
+{
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+
+    c = b;
+
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    
+    return 0;
+}
