1d5c3732b50a95c48d8fbf10abeee068fb6ed9e8
Hongbae Kim <hongbaki@c4c5c3.42berlin.de>
2023-12-11 17:38:30 +0100

    CPP/CPP08/ex02_added more code

--- CPP/CPP08/ex02/MutantStack.hpp
+++ CPP/CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: hongbaki <hongbaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:02:25 by hongbaki          #+#    #+#             */
-/*   Updated: 2023/12/11 17:27:04 by hongbaki         ###   ########.fr       */
+/*   Updated: 2023/12/11 17:37:58 by hongbaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,14 @@
 # define MUTANTSTACK_HPP
 
 # include <iostream>
-# include <exception>
-# include <ctime>
-# include <stdlib.h>
+
+# include <stack>
+# include <deque>
 
 template<typename T>
-class MutantStack
+class MutantStack : public std::stack<T>
 {
     private:
-    
     
     public:
         MutantStack();
@@ -38,84 +37,46 @@ class MutantStack
         MutantStack(MutantStack<T> const &arr);
         MutantStack<T> &operator=(MutantStack<T> const &rhs);
         
+        typedef typename std::stack<T>::container_type container_type;    
+        typedef typename container_type::iterator iterator;
+        typedef typename container_type::const_iterator const_iterator;    
+        typedef typename container_type::reverse_iterator reverse_iterator;    
+        typedef typename container_type::const_reverse_iterator iterator; 
         
-        
-        
+        iterator begin() {return this->c.begin();}  
+        iterator end() {return this->c.end();}
+        const_iterator begin() const {return this->c.begin();}
+        const_iterator end() const {return this->c.end();}
+        reverse_iterator rbegin() const {return this->c.rbegin();}
+        reverse_iterator rend() const {return this->c.rend();}
+        const_reverse_iterator rbegin() const {return this->c.rbegin();}
+        const_reverse_iterator rend() const {return this->c.rend();}
+       
 };
 
-//Data member 'n' represents the size of the array 0 which is empty array
+
 template<typename T>
-MutantStack<T>::MutantStack(): n(0)
+MutantStack<T>::MutantStack(): std::stack<T>()
 {
-    //Allocates dynamic memory for an array of type T with size 0
-    array = new T[0];
+}
+
+template<typename T>
+MutantStack<T>::MutantStack(MutantStack<T> const &ms): std::stack<T>(static_cast<std::stack<T> >(ms))
+{
 }
 
 template<typename T>
 MutantStack<T>::~MutantStack()
 {
-    delete[] array;
-}
-
-//This constructor for the Array class template is the parameterized constructor, 
-//and it is used to create an array with a specified size. 
-template<typename T>
-MutantStack<T>::MutantStack(unsigned int arraysize): n(arraysize)
-{
-    //Allocates dynamic memory for an array of type T with the specified size n
-    MutantStack = new T[n];
-    for (size_t i = 0; i < n; i++)
-    {
-        //Initializes each element of the array to the default value of type T
-        MutantStack[i] = T();
-    }
-}
-
-//The copy constructor creates a new object of the Array class 
-//that is a copy of another Array object (arr). 
-//The new object has its own dynamic memory allocated,
-// and the elements are copied from the source array.
-template<typename T>
-MutantStack<T>::MutantStack(MutantStack<T> const &arr) : n(arr.size())
-{
-    MutantStack = new T[arr.size()];
-    for (size_t i = 0; i < n; i++)
-        MutantStack[i] = arr.MutantStack[i];
 }
 
 
 template<typename T>
-MutantStack<T> &MutantStack<T>::operator=(Array<T> const &rhs)
+MutantStack<T> &MutantStack<T>::operator=(MutantStack<T> const &rhs)
 {
     if (this != &rhs) //Check for self-assignment
-    {
-        if (array)
-            delete[] array; // Deallocate existing memory
-        array = new T[rhs.size()]; // Allocate new memory
-        n = rhs.size(); // Update size
-        for (unsigned int i = 0; i < n; i++)
-            array[i] = rhs[i]; // Copy elements
-    }
+        static_cast<std::stack<T> >(*this) = static_cast<std::stack<T> >(rhs);
     return *this; //Return the current object
-}
-
-//[] operator overload for your Array class.
-template<typename T>
-T &Array<T>::operator[](unsigned int i)
-{
-    //If the index is out of bounds, an OutOfRangeException exception is thrown. 
-    if (i >= n)
-        throw(OutOfRangeException());
-    //If the index is within bounds, 
-    //the method returns a reference to the element at the specified index in the array.
-    return array[i];
-}
-
-//Number of elements dynamic array managed by Array class.
-template<typename T>
-unsigned int Array<T>::size() const
-{
-    return n;
 }
 
 #endif
