bcf94d1da4b910036e42cd6f843b288bb793306b
HONGBAEKIM <hongpei17@gmail.com>
2023-12-12 20:47:50 +0100

    CPP/CPP08/ex02_added detail explanation for code

--- CPP/CPP08/ex02/MutantStack.hpp
+++ CPP/CPP08/ex02/MutantStack.hpp
@@ -10,23 +10,22 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-//By completing this exercise, 
-//you will gain practical experience in 
-//implementing class templates, 
-//working with dynamic memory, handling exceptions, 
-//and designing a class that manages an array of elements. 
-//It's a good opportunity to reinforce your understanding of C++ features 
-//and best practices.
-
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
+
+//A template in C++ is a mechanism for creating generic classes or functions. 
+
+//A container in C++ refers to a class template or a data structure that can store 
+//and organize multiple elements of the same or different types. 
 
 # include <iostream>
 
 # include <stack>
+//deque is container which is data structure (double-ended queue)
 # include <deque>
 
 template<typename T>
+//MutantStack inherits std::stack<T>
 class MutantStack : public std::stack<T>
 {
     private:
@@ -37,12 +36,23 @@ class MutantStack : public std::stack<T>
         MutantStack(MutantStack<T> const &arr);
         MutantStack<T> &operator=(MutantStack<T> const &rhs);
         
-        typedef typename std::stack<T>::container_type container_type;    
+        //'std::stack<T>::container_type' is dependent name
+        //it depends on the templete parameter <T>
+        typedef typename std::stack<T>::container_type container_type;
+        
+        //You can use it to read and write (modify) the values in the iterator
         typedef typename container_type::iterator iterator;
+        //You can use it to read values from the const_iterator but cannot modify the values.
         typedef typename container_type::const_iterator const_iterator;    
         typedef typename container_type::reverse_iterator reverse_iterator;    
         typedef typename container_type::const_reverse_iterator const_reverse_iterator; 
         
+        /*
+        begin(): Returns an iterator pointing to the first element of the container.
+        end(): Returns an iterator pointing one past the last element of the container.
+        rbegin(): Returns a reverse iterator pointing to the last element of the container.
+        rend(): Returns a reverse iterator pointing to one position before the first element of the container.
+        */
         iterator begin() {return this->c.begin();}  
         iterator end() {return this->c.end();}
         const_iterator begin() const {return this->c.begin();}
@@ -51,15 +61,22 @@ class MutantStack : public std::stack<T>
         reverse_iterator rend() {return this->c.rend();}
         const_reverse_iterator rbegin() const {return this->c.rbegin();}
         const_reverse_iterator rend() const {return this->c.rend();}
-       
 };
 
-
+//when you create an instance of MutantStack<T>, 
+//the constructor initializes the std::stack<T> base class part 
+//using its default constructor.
 template<typename T>
 MutantStack<T>::MutantStack(): std::stack<T>()
 {
 }
 
+//copy constructor
+//the copy constructor of MutantStack<T> is constructing 
+//a new instance by copying another MutantStack<T> (ms). 
+//During this process, it ensures that the base class part (std::stack<T>) 
+//is correctly constructed using the copy constructor of std::stack<T>. 
+//The const & ensures that the original ms object is not modified during this copy process.
 template<typename T>
 MutantStack<T>::MutantStack(MutantStack<T> const &ms): std::stack<T>(static_cast<std::stack<T> >(ms))
 {
@@ -70,12 +87,16 @@ MutantStack<T>::~MutantStack()
 {
 }
 
-
+//copy assignment operator
 template<typename T>
 MutantStack<T> &MutantStack<T>::operator=(MutantStack<T> const &rhs)
 {
     if (this != &rhs) //Check for self-assignment
+    {
+        //Assigns the contents of rhs to the current object. 
+        //This involves using the copy assignment operator of std::stack<T>
         static_cast<std::stack<T> >(*this) = static_cast<std::stack<T> >(rhs);
+    }
     return *this; //Return the current object
 }
 
--- CPP/CPP08/ex02/main.cpp
+++ CPP/CPP08/ex02/main.cpp
@@ -14,23 +14,30 @@
 
 int main()
 {
+	//Creates an instance of MutantStack<int>
+	//it's initializing the 'std::stack' part of the 'MutantStack' using its default constructor.
 	MutantStack<int> mstack;
 
-	mstack.push(5);
-	mstack.push(17);
+	//Push: This operation adds an element to the top of the stack.
+	//Pop: This operation removes the element from the top of the stack.
+	mstack.push(0);
+	mstack.push(1);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "mstack.top() : " << mstack.top() << std::endl;
+	std::cout << "mstack.size() : " << mstack.size() << std::endl;
 
 	mstack.pop();
+	std::cout << "mstack.top() : " << mstack.top() << std::endl;
+	std::cout << "mstack.size() : " << mstack.size() << std::endl;
 
-	std::cout << mstack.size() << std::endl;
-
+	mstack.push(2);
 	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
+	mstack.push(4);
 	//[...]
-	mstack.push(0);
+	mstack.push(5);
 
+	//The begin() function returns an iterator pointing 
+	//to the first element of the container.
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 
@@ -39,9 +46,16 @@ int main()
 
 	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		//Print the current element pointed to by the iterator
+		std::cout << "*it : " << *it << std::endl;
 		++it;
 	}
+
+
+	//s is a new stack that is created and initialized with the contents of mstack. 
+	//The elements from mstack are copied into the new stack s
+	//you can create a standard stack (std::stack) 
+	//using your custom container mstack
 	std::stack<int> s(mstack);
 
 	return 0;
